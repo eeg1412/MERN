5a046abd6e4334bef1145ae29a81412710f1f05f
eeg1412 <eeg1412@gmail.com>
2020-10-17 18:09:11 +0900

    登陆注册

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import store from './store/data'
+import App from './view/App'
+import register from './view/register'
+
+import './assets/css/style.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  <Provider store={store}>
+    <Router>
+      <Route path="/" exact component={App}></Route>
+      <Route path="/register" component={register}></Route>
+    </Router>
+  </Provider>,
+  document.querySelector("#root")
+)
---
+++ client/src/view/register.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
+class register extends Component {
+    render () {
+        return (
+            <div>
+                <h1>注册</h1>
+                <div>
+                    <p>账号：<input type="text" /></p>
+                    <p>密码：<input type="password" /></p>
+                </div>
+                <div><button>注册</button><Link to="/">返回</Link></div>
+            </div>
+        );
+    }
+}
+
+export default register;
